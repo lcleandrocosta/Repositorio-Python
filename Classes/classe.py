2cc5b1e4a8ad194ab7f679b037dcef8afc77a7e2
Leandro Costa <75191456+lcleandrocosta@users.noreply.github.com>
2020-12-02 21:21:03 -0300

    Criação da classe Pessoa
    Criação da primeira classe com seus atributos.

---
+++ Classes/classe.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    def __init__(self, nome, sexo, cor, idade):
+        self.nome = nome
+        self.sexo = sexo
+        self.cor = cor
+        self.idade = idade
+
+    print('Minha primeira classe, "Pessoa"')
